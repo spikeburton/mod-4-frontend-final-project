50498b651a0c9490ee8ff5eabac02e140868816e
github username <github email address>
2019-04-11 17:11:28 -0400

    car components and containers and stuff

---
+++ src/components/CarStats.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CarStats extends React.Component {
+
+    render() {
+        return (
+
+            <div>
+                <form>
+                    this is a form
+                </form>
+            </div>
+        )
+    }
+}
+
+export default CarStats;
--- src/containers/CarImageContainer.js
+++ src/containers/CarImageContainer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import CarImageList from '../components/CarImageList'
 
 class CarImageContainer extends React.Component {
     render() {
         return (
             <div>
-                
+                <CarImageList />
             </div>
         )
     }
