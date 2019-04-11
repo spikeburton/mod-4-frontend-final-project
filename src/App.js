3eff98a03e8755e85eac7898e3a215c3d791ac4c
github username <github email address>
2019-04-11 17:19:04 -0400

    containers and components complete except map

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component, Fragment } from 'react';
 import {BrowserRouter as Router, Route } from "react-router-dom"
 import Login from "./components/Login"
 import SignUp from "./components/SignUp"
+import GameLoader from "./containers/GameLoader"
+import CarCreatorContainer from "./containers/CarCreatorContainer"
 
 
 class App extends Component {
@@ -11,6 +13,8 @@ class App extends Component {
                 <Fragment>
                     <Route exact path="/login" component={Login} />
                     <Route exact path="/signup" component={SignUp} />
+                    <Route exact path="/" component={GameLoader} />
+                    <Route exact path="/create" component={CarCreatorContainer} />
                 </Fragment>
             </Router>
         );
