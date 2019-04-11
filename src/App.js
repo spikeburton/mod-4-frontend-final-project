c2ad05a06b46cb178149064157cc8f8170bf51cf
spikeburton <ralphburton3@gmail.com>
2019-04-11 19:04:29 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React, { Component, Fragment } from 'react';
-import {BrowserRouter as Router, Route } from "react-router-dom"
-import Login from "./components/Login"
-import SignUp from "./components/SignUp"
-import GameLoader from "./containers/GameLoader"
-import CarCreatorContainer from "./containers/CarCreatorContainer"
-
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import GameLoader from "./containers/GameLoader";
+import CarCreatorContainer from "./containers/CarCreatorContainer";
 
 class App extends Component {
-    render() {
-        return (
-            <Router>
-                <Fragment>
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/signup" component={SignUp} />
-                    <Route exact path="/" component={GameLoader} />
-                    <Route exact path="/create" component={CarCreatorContainer} />
-                </Fragment>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/" component={GameLoader} />
+          <Route exact path="/create" component={CarCreatorContainer} />
+        </Fragment>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/containers/GameLoader.js
+++ src/containers/GameLoader.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import ScoreBoardContainer from "./ScoreBoardContainer"
-import CarContainer from "./CarContainer"
-import StartGameButton from "../components/StartGameButton"
+import React, { Component } from "react";
+import { Segment } from "semantic-ui-react";
+import ScoreBoardContainer from "./ScoreBoardContainer";
+import CarContainer from "./CarContainer";
+import StartGameButton from "../components/StartGameButton";
 
-class GameLoader extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <ScoreBoardContainer />
-                <CarContainer />
-                <StartGameButton />
-            </div>
-        )
-    }
+class GameLoader extends Component {
+  render() {
+    return (
+      <Segment.Group>
+        <Segment>
+          <ScoreBoardContainer />
+          <CarContainer />
+        </Segment>
+        <StartGameButton />
+      </Segment.Group>
+    );
+  }
 }
 
 export default GameLoader;
