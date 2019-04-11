c7d67412ba99eb5406bc999220a98577ebb566bb
github username <github email address>
2019-04-11 16:32:01 -0400

    added react dom router to dependencies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route } from "react-router-dom"
 // import logo from './logo.svg';
 // import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router></Router>
+        );
+    }
 }
 
 export default App;
