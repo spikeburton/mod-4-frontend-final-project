fd2f2bd41bc4d9150cb448e3ce72b931fb144a8f
spikeburton <ralphburton3@gmail.com>
2019-04-12 10:53:06 -0400

    layout car stats container

--- src/containers/CarStatsContainer.js
+++ src/containers/CarStatsContainer.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Segment } from "semantic-ui-react";
+import CarStats from "../components/CarStats";
 
 class CarStatsContainer extends React.Component {
-
-    render() {
-        return (
-
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Segment.Group>
+        <Segment>
+          <h2>Car Stats</h2>
+        </Segment>
+        <Segment id="car-stats-list">
+          <CarStats />
+        </Segment>
+      </Segment.Group>
+    );
+  }
 }
 
 export default CarStatsContainer;
