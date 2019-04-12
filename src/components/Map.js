15b9f72cf316c66c9643d729af66698bb10f721b
spikeburton <ralphburton3@gmail.com>
2019-04-12 14:51:23 -0400

    add map image to game container

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import { Image, Container } from "semantic-ui-react";
-import "../stylesheets/GameContainer/GameContainer.css";
-import mapPic3 from '../data.js'
+import React from "react";
+// import "../stylesheets/GameContainer/GameContainer.css";
+// import { mapPic3 } from "../data.js";
 
 class Map extends React.Component {
-
-
-    render() {
-        return (
-          <div id="map-container">
-            
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div id="map-container">
+        {/* Car will go here */}
+      </div>
+    );
+  }
 }
 
 export default Map;
