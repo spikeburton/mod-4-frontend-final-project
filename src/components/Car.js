2d8548dd8f8d568d3da939c5038f6b05dde1a9aa
spikeburton <ralphburton3@gmail.com>
2019-04-12 15:05:05 -0400

    add car component

---
+++ src/components/Car.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Car = props => {
+  return (
+    <div id="car">
+      <img src="https://cdn4.iconfinder.com/data/icons/race-cartoon/512/val42_2-128.png" alt="car"/>
+    </div>
+  )
+}
+
+export default Car
