bc911b04d828a20e60a87f087261fa8690974b4b
spikeburton <ralphburton3@gmail.com>
2019-04-12 10:28:59 -0400

    layout car image container

--- src/containers/CarImageContainer.js
+++ src/containers/CarImageContainer.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import CarImageList from '../components/CarImageList'
+import React from "react";
+import CarImageList from "../components/CarImageList";
+import { Segment } from "semantic-ui-react";
 
 class CarImageContainer extends React.Component {
-    render() {
-        return (
-            <div>
-                <CarImageList />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Segment.Group>
+        <Segment>
+          <h2>Car Type</h2>
+        </Segment>
+        <Segment placeholder id="car-image-list">
+          <CarImageList />
+        </Segment>
+      </Segment.Group>
+    );
+  }
 }
 
 export default CarImageContainer;
