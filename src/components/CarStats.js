954ae609154367e7327194e126094154ca5335e4
spikeburton <ralphburton3@gmail.com>
2019-04-12 11:43:23 -0400

    finish wiring up car stats

--- src/components/CarStats.js
+++ src/components/CarStats.js
@@ -1,18 +1,72 @@
-import React from "react";
-import { Segment, Form } from "semantic-ui-react";
+import React, { Component } from "react";
+import { Segment, Form, Grid, Image } from "semantic-ui-react";
 
-class CarStats extends React.Component {
+const DEFAULT_STATS = {
+  max_fuel: 50,
+  tread_wear: 50,
+  health: 50
+};
+
+class CarStats extends Component {
+  constructor() {
+    super();
+    this.state = DEFAULT_STATS;
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleDoubleClick = e => {
+    this.setState({
+      [e.target.name]: DEFAULT_STATS[e.target.name]
+    });
+  };
+
   render() {
     return (
       <Segment as={Form} id="car-stats-form">
-        {/* <form>this is a form</form> */}
-        <Form.Input
-          label="Max Fuel:"
-          name="max_fuel"
-          type="range"
-          min={0}
-          max={100}
-        />
+        <Grid columns={2}>
+          <Grid.Row verticalAlign="middle">
+            <Grid.Column>
+              <Image src="http://images.clipartpanda.com/car-top-view-clipart-red-racing-car-top-view-fe3a.png" />
+            </Grid.Column>
+            <Grid.Column>
+              <Form.Input
+                label={`Max Fuel: ${this.state.max_fuel}`}
+                name="max_fuel"
+                type="range"
+                min={0}
+                max={100}
+                value={this.state.max_fuel}
+                onChange={this.handleChange}
+                onDoubleClick={this.handleDoubleClick}
+              />
+              <Form.Input
+                label={`Tread Wear: ${this.state.tread_wear}`}
+                name="tread_wear"
+                type="range"
+                min={0}
+                max={100}
+                value={this.state.tread_wear}
+                onChange={this.handleChange}
+                onDoubleClick={this.handleDoubleClick}
+              />
+              <Form.Input
+                label={`Health: ${this.state.health}`}
+                name="health"
+                type="range"
+                min={0}
+                max={100}
+                value={this.state.health}
+                onChange={this.handleChange}
+                onDoubleClick={this.handleDoubleClick}
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Segment>
     );
   }
