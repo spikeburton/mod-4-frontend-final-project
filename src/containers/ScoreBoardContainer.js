fb05753eb323b21ca7ba3e213cec0d187ddc3605
spikeburton <ralphburton3@gmail.com>
2019-04-11 20:35:33 -0400

    add scores to state in scoreboard container

--- src/containers/ScoreBoardContainer.js
+++ src/containers/ScoreBoardContainer.js
@@ -1,9 +1,24 @@
 import React from "react";
 import ScoreBoardList from "../components/ScoreBoardList";
 
+const API = "http://localhost:3000";
+
 class ScoreBoardContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      scores: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(`${API}/scores`)
+      .then(response => response.json())
+      .then(scores => this.setState({ scores }));
+  }
+
   render() {
-    return <ScoreBoardList />;
+    return <ScoreBoardList scores={this.state.scores} />;
   }
 }
 
