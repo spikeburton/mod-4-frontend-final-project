dc9a0f8cdca44cbf64db1a199b57d17a7cfa865a
spikeburton <ralphburton3@gmail.com>
2019-04-12 11:21:18 -0400

    start layout for car stats form

--- src/components/CarStats.js
+++ src/components/CarStats.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Segment, Form } from "semantic-ui-react";
 
 class CarStats extends React.Component {
-
-    render() {
-        return (
-
-            <div>
-                <form>
-                    this is a form
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Segment as={Form} id="car-stats-form">
+        {/* <form>this is a form</form> */}
+        <Form.Input
+          label="Max Fuel:"
+          name="max_fuel"
+          type="range"
+          min={0}
+          max={100}
+        />
+      </Segment>
+    );
+  }
 }
 
 export default CarStats;
