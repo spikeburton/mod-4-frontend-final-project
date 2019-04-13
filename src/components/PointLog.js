20671f0bf34708335acee7bd78e0ef751d94665c
github username <github email address>
2019-04-13 09:40:36 -0400

    added-installed fontawesome icons and add gas-tire-heart-coins for game

--- src/components/PointLog.js
+++ src/components/PointLog.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { Segment } from "semantic-ui-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoins } from "@fortawesome/free-solid-svg-icons";
 
 class PointLog extends React.Component {
   render() {
-    return <Segment color="green">Point Log</Segment>;
+    return (
+      <Segment color="green">
+        <FontAwesomeIcon icon={faCoins} size="lg" />  Point Log
+      </Segment>
+    ); 
   }
 }
 
--- src/components/RealTimeGameStats.js
+++ src/components/RealTimeGameStats.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Segment } from "semantic-ui-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGasPump, faCircleNotch, faHeart } from "@fortawesome/free-solid-svg-icons";
+
+
+
 
 class RealTimeGameStats extends React.Component {
 
+
     render() {
         return (
-            <Segment.Group horizontal>
-              <Segment color="red">Fuel: 0</Segment>
-              <Segment color="red">Tire Tread: 0</Segment>
-              <Segment color="red">Health: 0</Segment>
-            </Segment.Group>
+          <Segment.Group horizontal>
+            <Segment color="red">
+              <FontAwesomeIcon icon={faGasPump} size="lg" /> Fuel: 0
+            </Segment>
+            <Segment color="red">
+              <FontAwesomeIcon icon={faCircleNotch} size="lg" spin />  Tire Tread: 0
+            </Segment>
+            <Segment color="red">
+              <FontAwesomeIcon icon={faHeart} size="lg" />  Health: 0
+            </Segment>
+          </Segment.Group>
         );
     }
 }
