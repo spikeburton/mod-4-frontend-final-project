329d1badfc10dcadb7162890bfc7b82d8ab69ce1
github username <github email address>
2019-04-13 01:00:53 -0400

    added login semantic - need image

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,41 @@
 import React from 'react';
-
+import { Button, Form, Grid, Header, Image, Message, Segment } from "semantic-ui-react";
 
 class Login extends React.Component {
 
     render() {
         return (
-            <div>
-                <form onSubmit={(e) => {e.preventDefault(); console.log("Submited Login") }} >
-                    <input name="username" type="text" label="Username" placeholder="Username..." />
-                    <input name="password" type="password" label="Password" placeholder="Password..." />
-                    <input type="submit" value="Login" />
-                </form>
+            <div className='login-form'>
+            <style>{`body > div, body > div > div, body > div > div > div.login-form { height: 100%; }`}</style>
+                <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+                    <Grid.Column style={{ maxWidth: 450 }}>
+
+                        <Header as='h2' color='green' textAlign='center'>
+                            <Image src='/logo.png' /> Log-in to your account
+                        </Header>
+
+                        <Form size='large'>
+                            <Segment stacked>
+                                <Form.Input fluid icon='user' iconPosition='left' placeholder='Email address...' />
+                                <Form.Input fluid icon='lock' iconPosition='left' placeholder='Password...' type='password' />
+                                <Button color='green' fluid size='large'>Login</Button>
+                            </Segment>
+                        </Form>
+
+                        <Message>Never Played? <a href='/signup'> Create Account!</a></Message>
+
+                    </Grid.Column>
+                </Grid>
             </div>
         )
     }
 }
-
 export default Login
+
+            // <div>
+            //     <form onSubmit={(e) => {e.preventDefault(); console.log("Submited Login") }} >
+            //         <input name="username" type="text" label="Username" placeholder="Username..." />
+            //         <input name="password" type="password" label="Password" placeholder="Password..." />
+            //         <input type="submit" value="Login" />
+            //     </form>
+            // </div>
