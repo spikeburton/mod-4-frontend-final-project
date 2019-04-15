a30043b28f6bdede01595fde49c3e10f3357e55d
github username <github email address>
2019-04-15 12:50:17 -0400

    added more to collision logic

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -15,7 +15,7 @@ class GameContainer extends React.Component {
         x: 292,
         y: 0
       },
-      boundaries: [] 
+      boundaries: []
     };
   }
 
@@ -51,6 +51,9 @@ class GameContainer extends React.Component {
         default:
           console.error("WTF HAPPENED LOL");
       }
+      this.checkCollision(pos.x, pos.y)
+        ? console.log("theres been a collision")
+        : console.log("no collision");
 
       this.setState({
         car: {
@@ -58,7 +61,7 @@ class GameContainer extends React.Component {
           x: pos.x,
           y: pos.y
         }
-      })
+      });
     }
     // if (e.which === 38) {
     //   console.log("pressed UP")
@@ -67,25 +70,53 @@ class GameContainer extends React.Component {
 
   componentDidMount() {
     document.addEventListener("keydown", this.handleCarMove);
-    const bumperDivs = document.querySelectorAll('.grass-building') 
-    console.log(bumperDivs)
+    const bumperDivs = document.querySelectorAll(".grass-building");
+    console.log(bumperDivs);
     let divObjects = [];
     bumperDivs.forEach(div => {
-      let divObjStyle = { bottom: div.style.bottom, 
-                          left: div.style.left,
-                          width: div.style.width,
-                          height: div.style.height }
+      let divObjStyle = {
+        bottom: div.style.bottom,
+        left: div.style.left,
+        width: div.style.width,
+        height: div.style.height
+      };
       Object.keys(divObjStyle).map(function(key, i) {
-          divObjStyle[key] = parseInt(divObjStyle[key].split("px")[0])
-      })
-      divObjects.push(divObjStyle)
+        divObjStyle[key] = parseInt(divObjStyle[key].split("px")[0]);
+      });
+      divObjects.push(divObjStyle);
     });
-    this.setState({ boundaries: divObjects})
+    this.setState({ boundaries: divObjects });
   }
 
   componentWillUnmount() {
     document.removeEventListener("keydown", this.handleCarMove);
   }
+
+  checkCollision = (x, y) => {
+    // let hasJustCollided = false;
+    const carWidth = 20;
+    const carHeight = 40;
+    for (let div of this.state.boundaries) {
+      if (
+        div.left < x + carWidth &&
+        div.left + div.width > x &&
+        div.bottom < y + carHeight &&
+        div.height + div.bottom > y
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+  // var rect1 = {x: 5, y: 5, width: 50, height: 50}
+  // var rect2 = {x: 20, y: 10, width: 10, height: 10}
+
+  // if (rect1.x < rect2.x + rect2.width &&
+  //    rect1.x + rect1.width > rect2.x &&
+  //    rect1.y < rect2.y + rect2.height &&
+  //    rect1.y + rect1.height > rect2.y) {
+  //     // collision detected!
+  // }
 
   render() {
     return (
