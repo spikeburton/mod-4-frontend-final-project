915752ccb35d551bed93ddfd85f4b95fef9a68a4
github username <github email address>
2019-04-15 14:01:40 -0400

    update handleMove function for bounds checking

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -51,21 +51,18 @@ class GameContainer extends React.Component {
         default:
           console.error("WTF HAPPENED LOL");
       }
-      this.checkCollision(pos.x, pos.y)
-        ? console.log("theres been a collision")
-        : console.log("no collision");
-
-      this.setState({
-        car: {
-          ...this.state.car,
-          x: pos.x,
-          y: pos.y
-        }
-      });
+      if (this.checkCollision(pos.x, pos.y)) {
+        console.log("theres been a collision");
+      } else {
+        this.setState({
+          car: {
+            ...this.state.car,
+            x: pos.x,
+            y: pos.y
+          }
+        });
+      }
     }
-    // if (e.which === 38) {
-    //   console.log("pressed UP")
-    // }
   };
 
   componentDidMount() {
@@ -93,7 +90,6 @@ class GameContainer extends React.Component {
   }
 
   checkCollision = (x, y) => {
-    // let hasJustCollided = false;
     const carWidth = 20;
     const carHeight = 40;
     for (let div of this.state.boundaries) {
@@ -108,15 +104,6 @@ class GameContainer extends React.Component {
     }
     return false;
   };
-  // var rect1 = {x: 5, y: 5, width: 50, height: 50}
-  // var rect2 = {x: 20, y: 10, width: 10, height: 10}
-
-  // if (rect1.x < rect2.x + rect2.width &&
-  //    rect1.x + rect1.width > rect2.x &&
-  //    rect1.y < rect2.y + rect2.height &&
-  //    rect1.y + rect1.height > rect2.y) {
-  //     // collision detected!
-  // }
 
   render() {
     return (
