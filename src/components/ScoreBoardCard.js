d0d553a3bb43faec93d26fb4d1158a78a347d031
spikeburton <ralphburton3@gmail.com>
2019-04-16 22:54:37 -0400

    style leaderboard cards

--- src/components/ScoreBoardCard.js
+++ src/components/ScoreBoardCard.js
@@ -3,11 +3,11 @@ import { Card } from "semantic-ui-react";
 
 const ScoreBoardCard = props => {
   return (
-    <Card>
-      <h4>username: {props.username}</h4>
-      <p>car: {props.car}</p>
-      <p>points: <span className="points">{props.points}</span></p>
-    </Card>
+    <Card
+      header={props.username.toUpperCase()}
+      meta={`Points: ${props.points}`}
+      extra={props.car}
+    />
   );
 };
 
