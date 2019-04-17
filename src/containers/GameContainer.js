0dd8ceafe8430bb329a783ee38c485ffc73a0550
spikeburton <ralphburton3@gmail.com>
2019-04-17 00:50:26 -0400

    add points and save score when game ends

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -22,8 +22,9 @@ class GameContainer extends React.Component {
         data: null
       },
       moves: 0,
-      gameActive: false,
       boundaries: [],
+      points: 100,
+      gameActive: false,
       gameOver: false
     };
   }
@@ -176,6 +177,23 @@ class GameContainer extends React.Component {
     }
   };
 
+  saveScore = () => {
+    const score = {
+      user_id: parseInt(localStorage.getItem("user")),
+      car_id: parseInt(localStorage.getItem("car")),
+      points: this.state.points
+    }
+
+    return fetch(`${API}/scores`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(score)
+    })
+  }
+
   startGame = () => {
       console.log("game started")
     document.addEventListener("keydown", this.handleCarMove);
@@ -201,7 +219,8 @@ class GameContainer extends React.Component {
     document.removeEventListener("keydown", this.handleCarMove);
     clearInterval(this.fuel);
     this.setState({ gameActive: false, gameOver: true });
-    console.warn("YOU LOSE SUCKER");
+    this.saveScore()
+    // console.warn("YOU LOSE SUCKER");
   };
 
   componentDidMount() {
