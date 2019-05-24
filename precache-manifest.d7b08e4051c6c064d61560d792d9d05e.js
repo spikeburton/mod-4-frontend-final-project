8629328d919bf004fba08e972a8fafd40688d55f
spikeburton <ralphburton3@gmail.com>
2019-05-24 12:40:39 -0400

    Updates

---
+++ precache-manifest.d7b08e4051c6c064d61560d792d9d05e.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f93f84fc6578edb0446",
+    "url": "/mod-4-frontend-final-project/static/css/main.02fc6d8a.chunk.css"
+  },
+  {
+    "revision": "0f93f84fc6578edb0446",
+    "url": "/mod-4-frontend-final-project/static/js/main.30f082d4.chunk.js"
+  },
+  {
+    "revision": "26b35a7906754e10601c",
+    "url": "/mod-4-frontend-final-project/static/js/runtime~main.060293f4.js"
+  },
+  {
+    "revision": "77601aa823a339cc8d14",
+    "url": "/mod-4-frontend-final-project/static/css/2.4f565cb0.chunk.css"
+  },
+  {
+    "revision": "77601aa823a339cc8d14",
+    "url": "/mod-4-frontend-final-project/static/js/2.b2cd411e.chunk.js"
+  },
+  {
+    "revision": "e3f53cf74fe93641df1aee2512e94441",
+    "url": "/mod-4-frontend-final-project/static/media/map6-800x450-Best.e3f53cf7.jpg"
+  },
+  {
+    "revision": "6fedc198e9680425a568bb16f89daf7d",
+    "url": "/mod-4-frontend-final-project/static/media/gas-pump.6fedc198.png"
+  },
+  {
+    "revision": "42f7bbb03235b2c8cf9edcc66f7fe336",
+    "url": "/mod-4-frontend-final-project/static/media/red-cross.42f7bbb0.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/mod-4-frontend-final-project/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/mod-4-frontend-final-project/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/mod-4-frontend-final-project/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/mod-4-frontend-final-project/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/mod-4-frontend-final-project/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/mod-4-frontend-final-project/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/mod-4-frontend-final-project/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/mod-4-frontend-final-project/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/mod-4-frontend-final-project/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/mod-4-frontend-final-project/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/mod-4-frontend-final-project/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/mod-4-frontend-final-project/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/mod-4-frontend-final-project/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/mod-4-frontend-final-project/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/mod-4-frontend-final-project/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/mod-4-frontend-final-project/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "8bdacb4b3a212b26daa1b3dfe20ef6ce",
+    "url": "/mod-4-frontend-final-project/static/media/grass.8bdacb4b.png"
+  },
+  {
+    "revision": "9d204c5bd6d525afc6fa60fb7139a3b1",
+    "url": "/mod-4-frontend-final-project/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/mod-4-frontend-final-project/precache-manifest.d7b08e4051c6c064d61560d792d9d05e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/mod-4-frontend-final-project/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
